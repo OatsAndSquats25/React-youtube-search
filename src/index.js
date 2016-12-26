b2b112d84fa6de215a2611bb0cf608fade242c0d
Carl <donc_langlois@hotmail.com>
2016-12-26 15:01:36 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDL9dBckT_PKKHTPrdq4iScPyz6QZFd2Y8';
+
+YTSearch({key:API_KEY, term:'surfboards'}, function(data) {
+  console.log(data);
+})
 // Create a new component. Should produce some html
 const App = () => {
   return (
