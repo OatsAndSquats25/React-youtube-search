e01cc00336b12af4782a8eddd0d6222c59821988
Carl <donc_langlois@hotmail.com>
2016-12-26 15:57:09 -0500

    Display list of videos from youtube

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDL9dBckT_PKKHTPrdq4iScPyz6QZFd2Y8';
 
-YTSearch({key:API_KEY, term:'surfboards'}, function(data) {
-  console.log(data);
-})
-// Create a new component. Should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
 
-// take this component's generated HTML and put it in the DOM
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+      return (
+        <div>
+          <SearchBar />
+          <VideoList videos={this.state.videos} />
+        </div>
+      );
+    }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
